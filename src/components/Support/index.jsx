511c2e5238a9111dea4bd68a0f6f2b741f15dec6
Alexander Tewpin <alex.tewpin@gmail.com>
2016-02-29 16:08:07 +0300

    Font Awesome added

--- src/components/Support/index.jsx
+++ src/components/Support/index.jsx
@@ -4,17 +4,22 @@ export default function Support ({ newMessages }) {
   return (
     <div styleName='root'>
       <a href='#' styleName='item'>
-        <div styleName='item-icon'>
+        <div styleName='item-icon-bg'>
+          <i className='fa fa-comment fa-lg'></i>
           <div styleName='item-notification'>{newMessages}</div>
         </div>
         <div styleName='item-title'>Чат</div>
       </a>
       <a href='#' styleName='item'>
-        <div styleName='item-icon'></div>
+        <div styleName='item-icon-bg'>
+          <i className='fa fa-envelope fa-lg'></i>
+        </div>
         <div styleName='item-title'>Почта</div>
       </a>
       <a href='#' styleName='item'>
-        <div styleName='item-icon'></div>
+        <div styleName='item-icon-bg'>
+          <i className='fa fa-microphone fa-lg'></i>
+        </div>
         <div styleName='item-title'>Голос</div>
       </a>
     </div>
